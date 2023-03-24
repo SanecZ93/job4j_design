684e36a48b493bc91e242a42c6a09b4b3a47481c
SanecZ93 <zyryanovalex93@gmail.com>
2023-03-24 16:22:27 +0300

    Утверждения с примитивными типами [#504883]

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -9,12 +9,55 @@ class BoxTest {
     void isThisSphere() {
         Box box = new Box(0, 10);
         String name = box.whatsThis();
-        boolean rsl = box.isExist();
-        int vertex = box.getNumberOfVertices();
-        double area = box.getArea();
         assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isTetrahedron() {
+        Box box = new Box(4, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Tetrahedron");
+    }
+
+    @Test
+    void isTrue() {
+        Box box = new Box(0, 10);
+        boolean rsl = box.isExist();
         assertThat(rsl).isTrue();
+    }
+
+    @Test
+    void isFalse() {
+        Box box = new Box(-1, 10);
+        boolean rsl = box.isExist();
+        assertThat(rsl).isFalse();
+    }
+
+    @Test
+    void isVertex0() {
+        Box box = new Box(0, 10);
+        int vertex = box.getNumberOfVertices();
         assertThat(vertex).isEqualTo(0);
+    }
+
+    @Test
+    void isVertex4() {
+        Box box = new Box(4, 10);
+        int vertex = box.getNumberOfVertices();
+        assertThat(vertex).isEqualTo(4);
+    }
+
+    @Test
+    void isAreaSphere() {
+        Box box = new Box(0, 10);
+        double area = box.getArea();
         assertThat(area).isEqualTo(1256.6370614359173);
+    }
+
+    @Test
+    void isAreaTetrahedron() {
+        Box box = new Box(4, 10);
+        double area = box.getArea();
+        assertThat(area).isEqualTo(173.20508075688772);
     }
 }
