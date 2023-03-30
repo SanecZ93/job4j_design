9f09f5934e180bb6a2f39412fdb01491ebbb2886
SanecZ93 <zyryanovalex93@gmail.com>
2023-03-30 15:53:52 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/test/java/ru/job4j/generic/RoleStoreTest.java
@@ -0,0 +1,102 @@
+package ru.job4j.generic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RoleStoreTest {
+
+    @Test
+    void whenAddAndFindThenRoleIsPetr() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        Role result = store.findById("1");
+        assertThat(result.getRole()).isEqualTo("Petr");
+    }
+
+    @Test
+    void whenAddAndFindThenRoleIsNull() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        Role result = store.findById("10");
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void whenAddDuplicateAndFindRoleIsPetr() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.add(new Role("1", "Maxim"));
+        Role result = store.findById("1");
+        assertThat(result.getRole()).isEqualTo("Petr");
+    }
+
+    @Test
+    void whenReplaceThenRoleIsMaxim() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.replace("1", new Role("1", "Maxim"));
+        Role result = store.findById("1");
+        assertThat(result.getRole()).isEqualTo("Maxim");
+    }
+
+    @Test
+    void whenNoReplaceRoleThenNoChangeRole() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.replace("10", new Role("10", "Maxim"));
+        Role result = store.findById("1");
+        assertThat(result.getRole()).isEqualTo("Petr");
+    }
+
+    @Test
+    void whenDeleteRoleThenIsNull() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.delete("1");
+        Role result = store.findById("1");
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void whenNoDeleteRoleThenIsPetr() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.delete("10");
+        Role result = store.findById("1");
+        assertThat(result.getRole()).isEqualTo("Petr");
+    }
+
+    @Test
+    void whenReplaceOkThenTrue() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        boolean rsl = store.replace("1", new Role("1", "Maxim"));
+        assertThat(rsl).isTrue();
+    }
+
+    @Test
+    void whenReplaceNotOkThenFalse() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        boolean rsl = store.replace("10", new Role("10", "Maxim"));
+        assertThat(rsl).isFalse();
+    }
+
+    @Test
+    void whenDeleteOkThenTrue() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        boolean rsl = store.delete("1");
+        assertThat(rsl).isTrue();
+    }
+
+    @Test
+    void whenDeleteNotOkThenFalse() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        boolean rsl = store.delete("2");
+        assertThat(rsl).isFalse();
+    }
+
+}
