807e930906ed1e8265d4cb0e2474d93ce53d8f17
SanecZ93 <zyryanovalex93@gmail.com>
2023-05-22 11:30:33 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.tree;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class SimpleTreeTest {
+
+    @Test
+    void when6ElFindLastThen6() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(6)).isPresent();
+    }
+
+    @Test
+    void when0ElFindLastThen0() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(2, 3);
+        tree.add(3, 4);
+        tree.add(4, 5);
+        tree.add(5, 4);
+        tree.add(5, 0);
+        assertThat(tree.findBy(0)).isPresent();
+    }
+
+    @Test
+    void whenNullPointerException() {
+        Tree<Integer> tree = new SimpleTree<>(0);
+        assertThatThrownBy(() -> tree.add(0, null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void whenElFindNotExistThenOptionEmpty() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(tree.findBy(7)).isEmpty();
+    }
+
+    @Test
+    void whenChildExistOnLeafThenNotAdd() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.add(2, 6)).isFalse();
+    }
+}
